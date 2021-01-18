091bc452f299dc029c5bca36417c823928ea9d3c
Harsh Vitra <harshvitra@Harshs-MacBook-Pro.local>
2021-01-18 20:27:18 +0530

    Env, css reset and fonts setup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import json from "@rollup/plugin-json";
+import {config} from 'dotenv';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -42,6 +44,15 @@ export default {
     file: "public/build/bundle.js",
   },
   plugins: [
+    replace({
+      // stringify the object       
+      __myapp: JSON.stringify({
+        env: {
+          isProd: production,
+          ...config().parsed // attached the .env config
+        }
+      }),
+    }),
     svelte({
       compilerOptions: {
         // enable run-time checks when not in production
--- src/UserProfile/TreeCounter.svelte
+++ src/UserProfile/TreeCounter.svelte
@@ -2,25 +2,76 @@
     import UserProfileLoader from "./../../utils/contentLoaders/UserProfileLoader.svelte";
     const fetchProfileData = (async () => {
         const response = await fetch(
-            "https://app.plant-for-the-planet.org/app/profiles/prf_sFuRTP4aB8mhAxi3CzyFND8u"
+            `${__myapp.env.API_URL}/profiles/prf_sFuRTP4aB8mhAxi3CzyFND8u`
         );
         return await response.json();
     })();
 </script>
 
-<div class="treeCounter">
+<div class="treeCounterContainer">
     {#await fetchProfileData}
         <UserProfileLoader />
     {:then data}
-        <p>Data found {console.log(data)}</p>
+        
+        <div class="treeCounterComponent">
+            <div class="treeCounter">
+                <div class="textContainer">
+                    <p class="treecount"> {data.score.personal+data.score.received} </p>
+                    <p class="treecountLabel">Trees Planted</p>
+                </div>
+                <div class="textContainer">
+                    <p class="treecount"> {data.score.target} </p>
+                    <p class="treecountLabel">Target</p>
+                </div>
+            </div>
+            <div class="progressCounter">
+                
+            </div>
+        </div>
     {:catch error}
         <p>An error occurred!</p>
     {/await}
 </div>
 
 <style>
-    .treeCounter{
+    .treeCounterContainer{
         height: 420px;
         width: 420px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .treeCounterComponent{
+        height: 300px;
+        width: 300px;
+        background-image:radial-gradient(white 0%, white 60%, #47A8DC 100%);
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .treeCounter{
+        height: 266px;
+        width: 266px;
+        border-radius: 50%;
+        background-color: #23519B;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    .treecount{
+        font-size: 48px;
+        font-weight: bold;
+        text-align: center;
+        color: white;
+    }
+    .treecountLabel{
+        font-size: 13px;
+        font-weight: bold;
+        text-align: center;
+        color: white;
     }
 </style>
