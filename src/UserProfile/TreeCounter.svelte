1f8e6a1592b4e0ea57e2484721f7a392cb48c5e2
Harsh Vitra <harshvitra@Harshs-MacBook-Pro.local>
2021-01-19 15:53:29 +0530

    Progress bar

--- src/UserProfile/TreeCounter.svelte
+++ src/UserProfile/TreeCounter.svelte
@@ -1,7 +1,6 @@
 <script>
     import UserProfileLoader from "./../../utils/contentLoaders/UserProfileLoader.svelte";
-    import {getFormattedNumber} from "./../../utils/formatNumber"
-
+    import { getFormattedNumber } from "./../../utils/formatNumber";
     export let userguid;
     const fetchProfileData = (async () => {
         const response = await fetch(
@@ -10,6 +9,9 @@
         return await response.json();
     })();
 
+    let radius = 140;
+    let size = 154;
+    let circumference = 2 * Math.PI * radius;
 
 </script>
 
@@ -21,34 +23,53 @@
             <div class="treeCounterComponent">
                 <div class="treeCounter">
                     <div class="textContainer">
-                        <p class="treecount"> {getFormattedNumber(data.score.personal+data.score.received)} </p>
+                        <p class="treecount">
+                            {getFormattedNumber(
+                                data.score.personal + data.score.received
+                            )}
+                        </p>
                         <p class="treecountLabel">Trees Planted</p>
                     </div>
                     <div class="textContainer">
-                        <p class="treecount"> {getFormattedNumber(data.score.target)} </p>
+                        <p class="treecount">
+                            {getFormattedNumber(data.score.target)}
+                        </p>
                         <p class="treecountLabel">Target</p>
                     </div>
                 </div>
-                <!-- <div class="progressCounter">
-                    
-                </div> -->
+
+            <svg style={`width:${size*2}px; height:${size*2}px;position:absolute;`}>
+                <circle
+                cx={size}
+                cy={size}
+                r={radius}
+                stroke="#68b030"
+                stroke-linecap="round"
+                stroke-width="24"
+                transform={`rotate(-90,${size},${size})`}
+                stroke-dasharray={circumference}
+                stroke-dashoffset={circumference * (1- (data.score.personal + data.score.received)/data.score.target)}
+                fill="transparent"
+                />
+            </svg>
+
+
             </div>
-            <button class="primaryButton" on:click="">Plant Trees</button>
+            <button class="primaryButton" on:click>Plant Trees</button>
         </div>
-        
     {:catch error}
         <p>An error occurred!</p>
     {/await}
 </div>
 
 <style>
-    .mainContainer{
+    .mainContainer {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
     }
-    .treeCounterContainer{
+    .treeCounterContainer {
         height: 420px;
         width: 420px;
         display: flex;
@@ -56,44 +77,44 @@
         align-items: center;
         justify-content: center;
     }
-    .treeCounterComponent{
+    .treeCounterComponent {
         height: 300px;
         width: 300px;
-        background-image:radial-gradient(white 0%, white 60%, #47A8DC 100%);
+        background-image: radial-gradient(white 0%, white 60%, #47a8dc 100%);
         border-radius: 50%;
         display: flex;
         justify-content: center;
         align-items: center;
         position: relative;
     }
-    .treeCounter{
+    .treeCounter {
         height: 266px;
         width: 266px;
         border-radius: 50%;
-        background-color: #23519B;
+        background-color: #23519b;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         text-align: center;
     }
-    .treecount{
+    .treecount {
         font-size: 48px;
         font-weight: bold;
         text-align: center;
         color: white;
     }
-    .treecountLabel{
+    .treecountLabel {
         font-size: 13px;
         font-weight: bold;
         text-align: center;
         color: white;
         margin-top: 6px;
     }
-    .primaryButton{
+    .primaryButton {
         color: white;
         font-weight: bold;
-        background-color: #68B030;
+        background-color: #68b030;
         height: 52px;
         padding: 18px 36px;
         text-align: center;
@@ -101,13 +122,5 @@
         border-radius: 52px;
         min-width: 280px;
         margin-top: 24px;
-    }
-
-    .progressCounter{
-        position: absolute;
-        height: 266px;
-        width: 266px;
-        border-radius: 50%;
-        border: 24px solid #68B030;
     }
 </style>
