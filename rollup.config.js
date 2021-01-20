292c40253af9752e79146d93b55676244a506ffe
Harsh Vitra <harshvitra@Harshs-MacBook-Pro.local>
2021-01-20 17:05:55 +0530

    Revert

--- rollup.config.js
+++ rollup.config.js
@@ -56,8 +56,7 @@ export default {
     svelte({
       compilerOptions: {
         // enable run-time checks when not in production
-        dev: !production,
-        customElement: true
+        dev: !production
       },
     }),
     json(),
