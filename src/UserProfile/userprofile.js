6d2fbd349acffa67919939ef6d2a66ac5f91ecc4
Norbert Schuler <norbert.schuler@epublica.de>
2021-01-21 16:48:38 +0100

    retrieve script property

--- src/UserProfile/userprofile.js
+++ src/UserProfile/userprofile.js
@@ -1,9 +1,16 @@
 import App from "./App.svelte";
 
+function getUserguid() {
+  const scripts = document.getElementsByTagName('script');
+  const lastScript = scripts[scripts.length-1];
+  const scriptName = lastScript;
+  return scriptName.getAttribute('userguid');
+}
+ 
 const app = new App({
   target: document.body,
   props: {
-		userguid: 'prf_NYCgTC4KSiJU7B0Fq76D7ieM'
+		userguid: getUserguid() || 'prf_NYCgTC4KSiJU7B0Fq76D7ieM'
 	}
 });
 
