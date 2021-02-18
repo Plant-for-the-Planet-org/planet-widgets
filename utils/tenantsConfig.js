7abbd856edbce4de6e52f72833fddabfba356d74
Harsh Vitra <harshvitra@Harshs-MacBook-Pro.local>
2021-02-18 19:08:23 +0530

    Update tenant config

--- utils/tenantsConfig.js
+++ utils/tenantsConfig.js
@@ -1,8 +1,5 @@
 import tenantConfig from './../tenant.json';
 export default function getTenantConfig(tenantkey){
-    switch(tenantkey){
-        case 'ten_I9TW3ncG': return tenantConfig.planet;
-        case 'ten_3hEjJCBs': return tenantConfig.salesforce;
-        default: return planet;
-    }
+   const config =  tenantConfig.filter(tenant => tenant.id === tenantkey);
+   return config;
 }
