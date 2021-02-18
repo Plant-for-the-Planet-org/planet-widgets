ab2d7783817dd0959efac09d0ba754419ca53572
Harsh Vitra <harshvitra@Harshs-MacBook-Pro.local>
2021-02-18 13:03:12 +0530

    Replace tenant name with tenant key

--- utils/tenantsConfig.js
+++ utils/tenantsConfig.js
@@ -1,6 +1,6 @@
 import tenantConfig from './../tenant.json';
-export default function getTenantConfig(tenantName){
-    switch(tenantName){
+export default function getTenantConfig(tenantkey){
+    switch(tenantkey){
         case 'ten_I9TW3ncG': return tenantConfig.planet;
         case 'ten_3hEjJCBs': return tenantConfig.salesforce;
         default: return planet;
