a48a3f9122c1adc2b82cefce5ffab3592c891d96
rishsingh07 <rishabhshilpasingh@gmail.com>
2021-03-09 11:32:26 +0530

    default theme light after user switched, tenantkey feature added in url

--- utils/tenantsConfig.js
+++ utils/tenantsConfig.js
@@ -1,5 +1,5 @@
 import tenantConfig from './../tenant.json';
 export default function getTenantConfig(tenantkey){
-   const config =  tenantConfig.filter(tenant => tenant.id === tenantkey);
+   const config =  tenantConfig.filter(tenant => tenant.id.toLowerCase() === tenantkey.toLowerCase());
    return config[0];
 }
