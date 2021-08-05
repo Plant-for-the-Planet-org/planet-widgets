b4be76265271378af920b3402787f8fcf049b012
rishsingh07 <rishabhshilpasingh@gmail.com>
2021-08-05 19:12:45 +0530

    Changed logic, wrapped <a> in widget script

--- src/CookiePolicy/App.svelte
+++ src/CookiePolicy/App.svelte
@@ -1,96 +1,77 @@
 <svelte:options tag="tree-popup" immutable={true} />
+
 <script>
-    import enLocale from '../../public/data/locales/en.json';
-    import { writable } from '../../utils/localstorage'
-
-    export let show=true;
+    import { writable } from "../../utils/localstorage";
+    let cookiePolicy = localStorage.getItem("hidePlanetCookie");
     export let message;
-    export let locale = "en";
-    let language = [];
-    language["en"] = enLocale;
+    export let buttontext;
 
-
-const cookiePolicy = localStorage.getItem('hidePlanetCookie')
-    export const hideCookieNotification = writable(cookiePolicy)
-    hideCookieNotification.subscribe(value => {
-       localStorage.setItem('hidePlanetCookie', value === true ? true : false)
-       
-    })
-
-
-  function hideCookieNotice(){
-      show = false
-      hideCookieNotification.set
-      hideCookieNotification.update
-  }
-
+    export const hideCookieNotification = writable(cookiePolicy);
+    
+    function hideCookieNotice(){
+        hideCookieNotification.subscribe(value => {
+            localStorage.setItem('hidePlanetCookie', value=true)
+        })
+        cookiePolicy=true
+    }
     
 </script>
-{#if show}
-    <div class="cookiePolicy" >
-      <div class="policyNotice">
-        {message}
-        <a class="policyLink" href="https://www.plant-for-the-planet.org/en/footermenu/privacy-policy">
-          {language[locale].privacyPolicy}
-        </a>
-      </div>
-      <button
-        id={"cookieCloseButton"}
-        class="primaryButton"
-        on:click={hideCookieNotice}
-      >
-        {language[locale].acceptClose}
-      </button>
+
+{#if !cookiePolicy}
+    <div class="cookiePolicy">
+        <div class="policyNotice">
+            {@html message}
+        </div>
+        <button
+            id={"cookieCloseButton"}
+            class="primaryButton"
+            on:click={hideCookieNotice}
+        >
+            {buttontext}
+        </button>
     </div>
 {/if}
+
 <style>
-      @import "https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap";
+    @import "https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap";
 
-   .cookiePolicy {
-  position: fixed;
-  background-color: white;
-  box-shadow: 0px 3px 6px -3px ;
-  bottom: 20px;
-  left: 20px;
-  width: 280px;
-  border-radius: 4px;
-  padding: 20px;
-  z-index: 10;
-  font-family: "Raleway", sans-serif;
- 
-   }
-   .policyNotice {
-       text-align: center;
-  }
-  .policyLink {
-    font-weight: bold;
-    text-decoration: none;
-    color: #007a49;
-    display: inline;
-  }
+    .cookiePolicy {
+        position: fixed;
+        background-color: white;
+        box-shadow: 0px 3px 6px -3px;
+        bottom: 20px;
+        left: 20px;
+        width: 280px;
+        border-radius: 4px;
+        padding: 20px;
+        z-index: 10;
+        font-family: "Raleway", sans-serif;
+    }
+    .policyNotice {
+        text-align: center;
+    }
 
-  .primaryButton {
-    color: white;
-    font-weight: bold;
-    background-image: linear-gradient(97deg, #68b030 4%, #007a49 116%);
-    height: 40px;
-    padding: 0px;
-    text-align: center;
-    border: 0px;
-    border-radius: 40px;
-    min-width: 200px;
-    width: 100%;
-    margin-top: 24px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition-duration: 0.5s;
-    transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
-    text-decoration: none;
-  }
-  .primaryButton:hover {
-    transform: translateY(-7px);
-    cursor: pointer;
-  }
-
+    .primaryButton {
+        color: white;
+        font-weight: bold;
+        background-image: linear-gradient(97deg, #68b030 4%, #007a49 116%);
+        height: 40px;
+        padding: 0px;
+        text-align: center;
+        border: 0px;
+        border-radius: 40px;
+        min-width: 200px;
+        width: 100%;
+        margin-top: 24px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition-duration: 0.5s;
+        transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
+        text-decoration: none;
+    }
+    .primaryButton:hover {
+        transform: translateY(-7px);
+        cursor: pointer;
+    }
 </style>
