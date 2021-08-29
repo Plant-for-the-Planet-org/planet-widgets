5bd498d17edbc9ce9750c264aca5de92e4a36029
rishsingh07 <rishabhshilpasingh@gmail.com>
2021-08-29 19:39:32 +0530

    Cookie box shadow for dark theme

--- src/CookiePolicy/App.svelte
+++ src/CookiePolicy/App.svelte
@@ -64,7 +64,8 @@
         position: fixed;
         background-color: var(--bg-color);
         color: var(--text-color);
-        box-shadow: 0px 3px 6px -3px;
+        box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
+                    0 8px 16px -8px rgba(0, 0, 0, 0.3);
         bottom: 20px;
         left: 20px;
         width: 280px;
